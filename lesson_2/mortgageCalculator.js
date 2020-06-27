d38fa7f8c82b877d4db85bf2729743573d874e7e
Emily Ma <em.kayama@gmail.com>
2020-06-27 00:25:29 -0400

    moved MESSAGES object to json file

--- lesson_2/mortgageCalculator.js
+++ lesson_2/mortgageCalculator.js
@@ -1,42 +1,23 @@
 const readline = require("readline-sync");
-const MESSAGES = {
-  welcome: "Welcome to the Mortgage Calculator!",
-  loan: "Please enter your loan amount (ex: 450000)",
-  apr: "Please enter your APR (ex: 5 for 5%, 4.5 for 4.5%)",
-  invalid: "Invalid entry, please try again.",
-  duration: "Please enter the duration of your loan in months",
-  restart: "Would you like to start over? (y/n)",
-  yesOrNo: "Please enter y or n."
-}
-
-function invalidNum(num) {
-  return num.trimStart() === "" || Number.isNaN(Number(num)) || Number(num) < 0;
-}
-
-function messages(msg) {
-  return MESSAGES[msg];
-}
-
-function exit() {
-  prompt("restart");
-  let restartInput = readline.question().toLowerCase();
-  while (restartInput[0] !== "n" && restartInput[0] !== "y" || restartInput.length !== 1) {
-    prompt("yesOrNo");
-    restartInput = readline.question().toLowerCase();
-  }
-  return restartInput;
-  
-} 
+const MESSAGES = require("./mortgage_messages.json");
 
 function prompt(key) {
   let message = messages(key);
   console.log(`=> ${message}`);
 }
 
+function messages(msg) {
+  return MESSAGES[msg];
+}
+
+function invalidNum(num) {
+  return num.trimStart() === "" || Number.isNaN(Number(num)) || Number(num) < 0;
+}
+
 function getUserInput(inputType, validity) {
   prompt(inputType);
   let input = readline.question();
-  while(validity(input)) {
+  while (validity(input)) {
     prompt("invalid");
     prompt(inputType);
     input = readline.question();
@@ -44,33 +25,36 @@ function getUserInput(inputType, validity) {
   return input;
 }
 
-function calcMortgage(loanAmount, apr, duration) {
-  let monthlyInterestRate = apr / 100 / 12;
-  if (monthlyInterestRate === 0) {
-    return loanAmount / duration;
+function calcMortgage(loanAmt, apr, duration) {
+  let mthlyRate = apr / 100 / 12;
+  if (mthlyRate === 0) {
+    return loanAmt / duration;
   } else {
-    return loanAmount * (monthlyInterestRate / (1 - Math.pow((1 + monthlyInterestRate), (-duration))));
+    return loanAmt * (mthlyRate / (1 - Math.pow((1 + mthlyRate), (-duration))));
   }
 }
 
+function exit() {
+  prompt("restart");
+  let reInput = readline.question().toLowerCase();
+  while ((reInput[0] !== "n" && reInput[0] !== "y") || reInput.length !== 1) {
+    prompt("yesOrNo");
+    reInput = readline.question().toLowerCase();
+  }
+  return reInput;
+}
+
+//PROGRAM START
+
 prompt("welcome");
 
-while(true) {
+while (true) {
   const loanInput = getUserInput("loan", invalidNum);
   const aprInput = getUserInput("apr", invalidNum);
   const durationInput = getUserInput("duration", invalidNum);
 
-  console.log(`your calculated loan amount is $${calcMortgage(loanInput, aprInput, durationInput).toFixed(2)}`)
+  console.log(`your calculated loan amount is $${calcMortgage(loanInput, aprInput, durationInput).toFixed(2)}`);
 
   let result = exit();
   if ( result === "n") break;
-
 }
-
-
-
-
-
-
-
-
