9c2eb4d2918a134446deeb97f275bc42c2e70241
Emily Ma <em.kayama@gmail.com>
2020-06-30 13:41:02 -0400

    add score keeping in RPSLS.js

--- lesson_2/RPSLS.js
+++ lesson_2/RPSLS.js
@@ -1,5 +1,7 @@
 const readline = require("readline-sync");
 
+const ROUNDS_TO_WIN = 5;
+
 const VALID_CHOICES = {
   rock: ["rock", "r"],
   paper: ["paper", "p"],
@@ -21,28 +23,29 @@ function prompt(msg) {
 }
 
 function validityTest(input) {
-  return !Object.values(VALID_CHOICES).join().includes(input);
+  let validChoicesArr = [].concat(...Object.values(VALID_CHOICES));
+  return !validChoicesArr.includes(input);
 }
 
-function playerWins(choice, computerChoice) {
-  return WINNING_COMBOS[choice].includes(computerChoice);
+function determineWinner(player1, player2) {
+  return WINNING_COMBOS[convertInput(player1)].includes(player2);
 }
 
-function standardizeChoice(input) {
+function convertInput(input) {
   for (let [key, value] of Object.entries(VALID_CHOICES)) {
     if (value.includes(input)) return key;
   }
 }
 
 function getPlayerChoice() {
-  prompt(`Choose one: ${Object.keys(VALID_CHOICES).join(", ")}`);
-  let choice = readline.question().trim().toLowerCase();
+  prompt("Choose one: rock (r), paper (p), scissors (sc), lizard (l), spock (sp).");
+  let choice = readline.question().toLowerCase();
   
   while (validityTest(choice)) {
     prompt("Invalid input. Please choose rock, paper, scissors, spock, or lizard.");
-    choice = readline.question().trim().toLowerCase();
+    choice = readline.question().toLowerCase();
   }
-  return standardizeChoice(choice);
+  return convertInput(choice);
 }
 
 function getComputerChoice() {
@@ -53,19 +56,46 @@ function getComputerChoice() {
 
 function displayWinner(choice, compChoice) {
   prompt(`Player chose ${choice}, computer chose ${compChoice}.`);
-  if (playerWins(choice, compChoice)) {
+  if (determineWinner(choice, compChoice)) {
     prompt("You win!");
   } else if (choice === compChoice) {
     prompt("It's a tie!");
   } else {
     prompt("Computer wins!");
   }
+  prompt(`Player: ${playerCounter} Computer: ${computerCounter}`);
+  if (grandWinner.length !== 0) {
+    prompt(`${grandWinner} is the grand winner!`);
+  }
 }
 
-function playGame() {
+function updateScore(choice, compChoice) {
+  if (determineWinner(choice, compChoice)) {
+    playerCounter++;
+    if (playerCounter === ROUNDS_TO_WIN) {
+      grandWinner = "Player";
+    }
+  } else if (determineWinner(compChoice, choice)) {
+    computerCounter++;
+    if (computerCounter === ROUNDS_TO_WIN) {
+      grandWinner = "Computer";
+    }
+  }
+}
+
+function resetScore() {
+  playerCounter = 0;
+  computerCounter = 0;
+  grandWinner = "";
+}
+
+function playRound() {
     let playerChoice = getPlayerChoice();
     let computerChoice = getComputerChoice();
+    updateScore(playerChoice, computerChoice);
     displayWinner(playerChoice, computerChoice);
+    if (grandWinner.length !== 0) resetScore();
+    
 }
 
 function endGame() {
@@ -76,21 +106,17 @@ function endGame() {
     prompt("Please enter 'y' or 'n'");
     answer = readline.question().toLowerCase();
   }
-
+  console.clear();
   return answer[0] === "y" ? false : true;
 }
 
-function initializeGame() {
+  console.clear();
+  let playerCounter = 0;
+  let computerCounter = 0;
+  let grandWinner = "";
   prompt("Welcome to Rock Paper Scissors Lizard Spock!");
-  do {
-    playGame();
+  prompt("First to 5 is the grand winner.");
+  while (true) {
+    playRound();
     if (endGame()) break;
-  } while (true);
-}
-
-initializeGame();
-
-  
-  
-
-  
+  }
